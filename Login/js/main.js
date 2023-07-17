8e4e693493cde387bd3c8e0fe2e007380707cd71
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-07-18 00:55:31 +0300

    noti

--- Login/js/main.js
+++ Login/js/main.js
@@ -11,25 +11,51 @@ login.addEventListener("submit", (e) => {
     password: pas,
   };
 
-  const xhr = new XMLHttpRequest();
-  xhr.open(
-    "POST",
-    "http://localhost:8080/furryfinderfriends/api/v1/auth/login",
-    true
+  // let not = document.querySelector(".alert");
+  // if (!not) {
+  //   let bdy = document.getElementById("log-bdy");
+  //   let alert = document.createElement("div");
+  //   alert.classList.add("alert");
+  //   alert.textContent =
+  //     "wrong passworddddddddddddddddddddddddddddddddddddddddddddsssssssssssssssssssssssssssssssssssd";
+  //   bdy.appendChild(alert);
+  //   setTimeout(() => {
+  //     bdy.removeChild(alert);
+  //   }, 4000);
+  // }
+
+
+  localStorage.setItem(
+    "user",
+    JSON.stringify({
+      name: "ali",
+    })
   );
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        let response = JSON.parse(xhr.responseText);
-        let userData = response.data;
-        localStorage.setItem("user", JSON.stringify(userData));
-        window.location.href = "../home_page/index.html";
-      } else {
-        let x = JSON.parse(xhr.responseText);
-        alert(x.message);
-      }
-    }
-  };
-  xhr.send(JSON.stringify(user));
+  window.location.href = "../home_page/index.html";
+
+
+
+  
+  //Uncomment 14-34
+  // const xhr = new XMLHttpRequest();
+  // xhr.open(
+  //   "POST",
+  //   "http://localhost:8080/furryfinderfriends/api/v1/auth/login",
+  //   true
+  // );
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState === 4) {
+  //     if (xhr.status === 200) {
+  //       let response = JSON.parse(xhr.responseText);
+  //       let userData = response.data;
+  //       localStorage.setItem("user", JSON.stringify(userData));
+  //       window.location.href = "../home_page/index.html";
+  //     } else {
+  //       let x = JSON.parse(xhr.responseText);
+  //       alert(x.message);
+  //     }
+  //   }
+  // };
+  // xhr.send(JSON.stringify(user));
 });
